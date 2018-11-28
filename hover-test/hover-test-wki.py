91f8db60c8c0087bec9400d58484fa6dfb10bfe8
Ajaya Puri Sann <33626260+puriajaya@users.noreply.github.com>
2018-11-28 06:57:53 +0545

    Update hover-test-wki.py

--- hover-test/hover-test-wki.py
+++ hover-test/hover-test-wki.py
@@ -20,15 +20,18 @@ x_hover_on='//p/a[@title="Nepali language"]'
 
 driver.find_element_by_xpath(x_search).send_keys(keys[0])
 driver.find_element_by_xpath(x_submit).click()
-hover_text=driver.find_element_by_xpath(x_hover_on).text
+hover_text="<<NA>>"
+linked_data="<<NA>>"
 try:
     element = driver.find_element_by_xpath(x_hover_on)
+    hover_text=driver.find_element_by_xpath(x_hover_on).text
+    linked_data=driver.find_element_by_xpath(x_hover_on).get_attribute('href')
     hov = ActionChains(driver).move_to_element(element)
     hov.perform()
     time.sleep(10) 
 finally:
     print("Hovered on:",hover_text)
-    print("Linked Data",driver.find_element_by_xpath(x_hover_on).get_attribute('href'))
+    print("Linked Data",linked_data)
     driver.close()
 
 
